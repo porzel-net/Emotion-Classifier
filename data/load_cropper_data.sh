8080bd75317edee3f7ea5b03716f7a6174deec7d
juliusporzel <julius.porzel@web.de>
2026-02-18 21:50:56 +0100

    feat: added the data loader as sh script for simplicity

---
+++ data/load_cropper_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -o ./soloface-detection-dataset.zip\
+    https://zenodo.org/records/14474899/files/soloface-detection-dataset.zip?download=1
---
+++ data/load_facial_landmark_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -L -o ./cropped-face-keypoint-dataset-68-landmarks.zip\
+  https://www.kaggle.com/api/v1/datasets/download/sovitrath/cropped-face-keypoint-dataset-68-landmarks
